76c3c460dd7e83c9254880e276a83feeb700d38e
Paras Lohia <81807209+paras9192@users.noreply.github.com>
2022-10-09 16:43:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = reqire('express');
-const app = express()
+const app = express();
 
+//this is a get api
 app.get("/",(req,res))
 {
 res.send("Hello world,running on the port 3000");
